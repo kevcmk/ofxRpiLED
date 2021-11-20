9c05a4ee622f1b2e94366a7cd768ab392ee76cbd
Kevin Katz <kevcmk@gmail.com>
2021-11-19 21:15:11 -0800

    Soft reset to iizu-array

--- src/ofxRpiLED.cpp
+++ src/ofxRpiLED.cpp
@@ -1,5 +1,5 @@
 #include "ofxRpiLED.h"
-
+#include <stdexcept>
 
 // Constructor
 ofxRpiLED::ofxRpiLED() {}
@@ -11,33 +11,40 @@ ofxRpiLED::~ofxRpiLED() {
     delete matrix;
 }
 
-void ofxRpiLED::setup() {
-    RGBMatrix::Options options;
-    rgb_matrix::RuntimeOptions runtime;
-    options.hardware_mapping = "adafruit-hat";
-    options.rows = 32; // A 32x32 display. Use 16 when this is a 16x32 display.
-    options.chain_length = 4; // Number of boards chained together.
-    options.parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
-    options.brightness = 50;
-    setup(options, runtime);
-}
-
-void ofxRpiLED::setup(int _rows, int _chain, int _parallel) {
-    RGBMatrix::Options options;
-    rgb_matrix::RuntimeOptions runtime;
-    options.hardware_mapping = "adafruit-hat";
-    options.rows = _rows; // A 32x32 display. Use 16 when this is a 16x32 display.
-    options.chain_length = _chain; // Number of boards chained together.
-    options.parallel = _parallel; // Number of chains in parallel (1..3). > 1 for plus or Pi2
-    options.brightness = 50;
-    setup(options, runtime);
-}
-
-void ofxRpiLED::setup(RGBMatrix::Options options, rgb_matrix::RuntimeOptions runtime) {
+void ofxRpiLED::setup(std::string hardwareMapping, int columns, int rows, int chain, int parallel, int brightness, int gpioSlowdown) {
+    
 	/*
 	* Set up GPIO pins. This fails when not running as root.
 	*/
-    matrix = CreateMatrixFromOptions(options, runtime);
+    
+    rgb_matrix::RGBMatrix::Options options;
+    rgb_matrix::RuntimeOptions runtime;
+    
+    
+    
+    const std::string::size_type size = hardwareMapping.size();
+    char *buffer = new char[size + 1];   //we need extra char for NUL
+    memcpy(buffer, hardwareMapping.c_str(), size + 1);
+    
+    // This looks like it gets prematurely deallocated without a copy
+    options.hardware_mapping = buffer; // {hardware_mapping = 0x8358d0 "\030\270>\266",
+    cout << "Hardware Mapping: " << options.hardware_mapping << endl;
+    
+    
+    options.rows = rows; // Default
+    options.cols = columns; // Default
+    options.chain_length = chain;
+    options.parallel = parallel;
+    options.brightness = brightness;
+    // options.pixel_mapper_config = "Rotate:270";
+    runtime.gpio_slowdown = gpioSlowdown;
+    
+    std::string err = "";
+    if (!options.Validate(&err)) {
+        throw new std::invalid_argument("Invalid LED Options: " + err);
+    };
+    
+    matrix = rgb_matrix::CreateMatrixFromOptions(options, runtime);
     if (matrix == NULL) {
         cout << "Creating matrix failed!" << endl;
         exit(1);
@@ -52,19 +59,22 @@ void ofxRpiLED::setup(RGBMatrix::Options options, rgb_matrix::RuntimeOptions run
 	cH = matrix->height();
     
     off_screen_canvas = matrix->CreateFrameCanvas();
+    
+    canvas = matrix;
 
 }
 
 void ofxRpiLED::clear(){
-	matrix->Clear();
+	canvas->Clear();
 }
 
 void ofxRpiLED::draw(ofPixels &p){
 	int w = cW > p.getWidth()  ? cW : p.getWidth();
 	int h = cH > p.getHeight() ? cH : p.getHeight();	
-	for (unsigned int x = 0; x < w; x++) {
-		for (unsigned int y = 0; y < h; y++) {
+	for (int x = 0; x < w; x++) {
+		for (int y = 0; y < h; y++) {
 			ofColor c = p.getColor(x, y);
+//            matrix->SetPixel(x, y, c.r, c.g, c.b);;
             off_screen_canvas->SetPixel(x, y, c.r, c.g, c.b);
 		}
 	}
@@ -74,9 +84,10 @@ void ofxRpiLED::draw(ofPixels &p){
 void ofxRpiLED::draw(ofImage &i){
 	int w = cW > i.getWidth()  ? cW : i.getWidth();
 	int h = cH > i.getHeight() ? cH : i.getHeight();	
-	for (unsigned int x = 0; x < w; x++) {
+	for (int x = 0; x < w; x++) {
 		for (unsigned int y = 0; y < h; y++) {
 			ofColor c = i.getColor(x, y);
+//            matrix->SetPixel(x, y, c.r, c.g, c.b);
             off_screen_canvas->SetPixel(x, y, c.r, c.g, c.b);
 		}
 	}
